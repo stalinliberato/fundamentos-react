18edc9763b4a7ab1f3ca1868099af4086f6046ff
stalin.bessa <stalin.bessa@sipam.gov.br>
2024-11-05 11:53:04 -0400

    Exercicio 01: Primeira página React

---
+++ exercicio-01-pagina-html/src/App.jsx
@@ -0,0 +1,24 @@
+export default function App() {
+  return (
+    <div>
+      <img src="react-logo.svg" width="200" height="200" alt="Aprendendo React" />
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuários web e nativas</p>
+      <button>Aprenda React</button>
+      <button>Referência da API</button>
+      <hr />
+      <h2>Crie interfaces de usuários de componentes</h2>
+      <p>Componentes React são funções Javascript. A sintaxe de marcação é chamada de JSX. É uma extensão da sintaxe JavaScript popularizada pelo React</p>
+      <hr />
+      <h2>Próximos Passos</h2>
+      <ul>
+        <li>Uso de dados dinâmicos no JSX</li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos JavaScript</li>
+      </ul>
+    </div>
+
+  )
+}

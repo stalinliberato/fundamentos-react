78a5ca3608304a742d99f875500b360fb6f824ce
stalin.bessa <stalin.bessa@sipam.gov.br>
2024-11-07 15:34:38 -0400

    Utilizando Props

--- criando-card/src/App.jsx
+++ criando-card/src/App.jsx
@@ -1,10 +1,16 @@
+import rotposterImg from "./assets/star-wars.jpg"
+import swposterImg from "./assets/return-jedi.jpg"
+import pesposterImg from "./assets/empire.jpg"
 import Card from "./components/Card"
 
 export default function App() {
   return (
-    <div>
-       <Card />
-    </div>
+    <>
+      <Card title="Pôster: Start Wars (1977)" posterImg={rotposterImg}/>
+      <Card title="Pôster: Empire Stikes Back (1980)" posterImg={pesposterImg}/>
+      <Card title="Pôster: Return of the Jedi (1983)" posterImg={swposterImg}/>
+    </>
      
   )
 }
+
